125601fe24d4ca46267c83daf6940224921a7837
leedaju <dajoo417@naver.com>
2018-06-15 18:12:06 +0900

    Game1
    Print Game(Unity)

---
+++ print (4)/Assets/Scipt/BearController.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearController : MonoBehaviour {
+
+	GameObject p1;
+	GameObject p2;
+	GameObject p3;
+	GameObject p4;
+	GameObject Slot001;
+	GameObject Slot002;
+	GameObject Slot003;
+	GameObject Slot004;
+	GameObject bear;
+	GameObject question;
+	GameObject flag;
+
+	Rigidbody2D rigid2D;
+	float jumpforce1 = 200.0f;
+
+
+	float walkForce = 87.0f;
+	//float MaxWalkSpeed=2.0f;
+	int count = 0;
+
+	void jump(){
+		this.rigid2D.AddForce (new Vector2(this.walkForce, this.jumpforce1));
+		question.transform.Translate (1.25f, 0.5f, 0);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		this.rigid2D = GetComponent<Rigidbody2D> ();
+		this.p1 = GameObject.Find ("p1");
+		this.Slot001 = GameObject.Find ("Slot001");
+		this.p2 = GameObject.Find ("p2");
+		this.Slot002 = GameObject.Find ("Slot002");
+		this.p3 = GameObject.Find ("p3");
+		this.Slot003 = GameObject.Find ("Slot003");
+		this.p4 = GameObject.Find ("p4");
+		this.Slot004 = GameObject.Find ("Slot004"); 
+		this.bear = GameObject.Find ("bear");
+		this.question = GameObject.Find ("question");
+		this.flag = GameObject.Find ("flag");
+
+		question.SetActive (false);
+
+	}
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        LoadDB load = GameObject.Find("GameObject").GetComponent<LoadDB>();
+        int a = load.i;
+
+        if (a == 0)
+        {
+
+            if (count == 0)
+            {
+
+                if (this.p2.transform.position == this.Slot001.transform.position && count == 0)
+                {
+                    question.SetActive(false);
+                    count++;
+                    jump();
+
+                }
+                else if (this.p1.transform.position == this.Slot001.transform.position ||
+                         this.p3.transform.position == this.Slot001.transform.position ||
+                         this.p4.transform.position == this.Slot001.transform.position)
+                {
+
+                    question.SetActive(true);
+                }
+
+
+            }
+
+            if (this.p1.transform.position == this.Slot002.transform.position && count == 1)
+            {
+                question.SetActive(false);
+                count++;
+                jump();
+            }
+            else if (this.p2.transform.position == this.Slot002.transform.position ||
+                        this.p3.transform.position == this.Slot002.transform.position ||
+                     this.p4.transform.position == this.Slot002.transform.position)
+            {
+
+                question.SetActive(true);
+            }
+
+
+
+
+            if (this.p3.transform.position == this.Slot003.transform.position && count == 2)
+            {
+                question.SetActive(false);
+                count++;
+                jump();
+            }
+            else if (this.p1.transform.position == this.Slot003.transform.position ||
+                     this.p2.transform.position == this.Slot003.transform.position ||
+                     this.p4.transform.position == this.Slot003.transform.position)
+            {
+
+                question.SetActive(true);
+            }
+
+
+
+            if (this.p4.transform.position == this.Slot004.transform.position && count == 3)
+            {
+                question.SetActive(false);
+                count++;
+                jump();
+            }
+            else if (this.p1.transform.position == this.Slot004.transform.position ||
+                     this.p2.transform.position == this.Slot004.transform.position ||
+                     this.p3.transform.position == this.Slot004.transform.position)
+            {
+
+                question.SetActive(true);
+            }
+			if (count == 4) 
+			{
+				bump ();
+			}
+        }
+        else if (a == 1)
+        {
+            if (count == 0)
+            {
+
+                if (this.p1.transform.position == this.Slot001.transform.position && count == 0)
+                {
+                    question.SetActive(false);
+                    count++;
+                    jump();
+
+                }
+                else if (this.p2.transform.position == this.Slot001.transform.position ||
+                         this.p3.transform.position == this.Slot001.transform.position ||
+                         this.p4.transform.position == this.Slot001.transform.position)
+                {
+
+                    question.SetActive(true);
+                }
+
+
+            }
+
+            if (this.p3.transform.position == this.Slot002.transform.position && count == 1)
+            {
+                question.SetActive(false);
+                count++;
+                jump();
+            }
+            else if (this.p1.transform.position == this.Slot002.transform.position ||
+                        this.p2.transform.position == this.Slot002.transform.position ||
+                     this.p4.transform.position == this.Slot002.transform.position)
+            {
+
+                question.SetActive(true);
+            }
+
+
+
+
+            if (this.p4.transform.position == this.Slot003.transform.position && count == 2)
+            {
+                question.SetActive(false);
+                count++;
+                jump();
+            }
+            else if (this.p1.transform.position == this.Slot003.transform.position ||
+                     this.p2.transform.position == this.Slot003.transform.position ||
+                     this.p3.transform.position == this.Slot003.transform.position)
+            {
+
+                question.SetActive(true);
+            }
+
+
+
+            if (this.p2.transform.position == this.Slot004.transform.position && count == 3)
+            {
+                question.SetActive(false);
+                count++;
+                jump();
+            }
+            else if (this.p1.transform.position == this.Slot004.transform.position ||
+                     this.p3.transform.position == this.Slot004.transform.position ||
+                     this.p4.transform.position == this.Slot004.transform.position)
+            {
+
+                question.SetActive(true);
+            }
+			if (count == 4) 
+			{
+				bump ();
+			}
+
+        }
+
+
+        }
+
+	void bump(){
+
+		LoadDB load = GameObject.Find("GameObject").GetComponent<LoadDB>();
+
+		Vector2 p1 = this.flag.transform.position;
+		Vector2 p2 = this.bear.transform.position;
+		Vector2 dir = p1 - p2;
+		float d = dir.magnitude;
+		float r1 = 0.3f;
+		float r2 = 0.6f;
+
+		if (d < r1 + r2) {
+
+			//this.bear_up.SetActive (true);
+			this.flag.transform.Translate (0, 0.03f, 0);
+			StartCoroutine (load.LoadImg ());
+
+
+		}
+	}
+
+    } 
+
+
---
+++ print (4)/Assets/Scipt/LoadDB.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using UnityEngine.UI;
+
+
+public class LoadDB : MonoBehaviour
+{
+    // 싱글턴 인스턴스 선언
+    public static LoadDB instance = null;
+
+    // 데이터 가져오는 걸 요청하기 위한 PHP 주소
+    private string urlText = "http://chocohsu.cafe24.com/TestL.php";
+
+    // 텍스트 출력을 위한 객체 --> 배열로 바꾸기
+    public Text mText0, mText1, mText2, mText3;
+    public Text imgText, imgText2;
+    public int i;
+    // 이미지 출력을 위한 객체
+    public RawImage img;
+
+    void Awake()
+    {    // 싱글턴 인스턴스 할당
+        instance = this;
+    }
+
+    void Start()
+    {
+        StartCoroutine(GetData());
+        i = Random.Range(0, 2);
+    }
+
+    // 데이터 정보를 요청하는 코루틴 함수
+    public IEnumerator GetData()
+    {
+        WWW www = new WWW(urlText);
+        yield return www;
+
+        if (string.IsNullOrEmpty(www.error))
+        {
+       
+            // 데이터를 표시하는 함수 호출
+            DispData(www.text);
+        }
+        else
+        {
+            Debug.Log("Error : " + www.text);
+        }
+    }
+
+     
+    // JSON 파일을 파싱한 후 데이터를 표시
+    void DispData(string strJsonData)
+    {
+        var N = JSON.Parse(strJsonData);
+
+        // JSON 오브젝트의 배열에서 랜덤으로 선택
+
+        Debug.Log(i);
+        // 배열로 바꾸기
+        string syntax0 = N[i]["syntax0"].ToString();
+        string syntax1 = N[i]["syntax1"].ToString();
+        string syntax2 = N[i]["syntax2"].ToString();
+        string syntax3 = N[i]["syntax3"].ToString();
+    
+        Debug.Log(syntax0 + syntax1 + syntax2 + syntax3);
+
+  
+       
+
+
+        string s1 = syntax0.Replace("\"", "");
+        string s2 = s1.Replace("\\", "\"");
+
+        mText0.text = s2;
+
+        string s3 = syntax1.Replace("\"", "");
+        string s4 = s3.Replace("\\", "\"");
+
+        mText1.text = s4;
+
+
+        string s5 = syntax2.Replace("\"", "");
+        string s6 = s5.Replace("\\", "\"");
+
+        mText2.text = s6;
+
+        string s7 = syntax3.Replace("\"", "");
+        string s8 = s7.Replace("\\", "\"");
+
+        mText3.text = s8;
+        
+
+    }
+
+
+
+    public IEnumerator LoadImg()
+{
+        string ur = "";
+        if (i == 0)
+        {
+            ur = "http://chocohsu.cafe24.com/image/image1.jpg";
+        }
+        else
+        {
+            ur = "http://chocohsu.cafe24.com/image/image2.jpg";
+        }
+        WWW www = new WWW(ur);
+    yield return www;
+    Texture2D tt = www.texture;
+    img.texture = tt;
+}
+
+}
+
